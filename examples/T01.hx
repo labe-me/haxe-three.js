6db0cb0946967e19614ad7c876c4646beace5df6
Laurent Bédubourg <laurent@labe.me>
2011-12-29 14:42:15 +0100

    Cleanup

--- examples/T01.hx
+++ examples/T01.hx
@@ -5,10 +5,8 @@ class T01 {
     public static function main(){
         var w = Lib.window.innerWidth;
         var h = Lib.window.innerHeight;
-        var camera = new PerspectiveCamera(70, w/h, 1, 1000);
-        camera.position.z = 500;
+
         var scene = new Scene();
-        scene.add(camera);
         // create a red cube
         var material = new MeshLambertMaterial({color:0xcc0000});
         var geometry = new CubeGeometry(50, 50, 50, 1, 1, 1, material, null);
@@ -19,11 +17,15 @@ class T01 {
         var pointLight = new PointLight(0xffffff, 1, 0);
         pointLight.position.set(10, 50, 130);
         scene.add(pointLight);
+        // and a camera
+        var camera = new PerspectiveCamera(70, w/h, 1, 1000);
+        camera.position.z = 500;
+        scene.add(camera);
+        // setup renderer in the document
         var renderer = new WebGLRenderer(null);
-        renderer.setSize(w,h);
-        var view = Lib.document.createElement("div");
-        Lib.document.body.appendChild(view);
-        view.appendChild(renderer.domElement);
+        renderer.setSize(w, h);
+        Lib.document.body.appendChild(renderer.domElement);
+        // refresh once in a while
         var timer = new haxe.Timer(30);
         timer.run = function(){
             renderer.render(scene, camera, null, null);
