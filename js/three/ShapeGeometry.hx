d0b1df09e4820b298c06a3972b49aba6a3fd1c52
Laurent Bedubourg <laurent@labe.me>
2014-02-16 23:34:07 +0100

    ShapeGeometry extends Geometry

--- js/three/ShapeGeometry.hx
+++ js/three/ShapeGeometry.hx
@@ -1,6 +1,6 @@
 package js.three;
 
 @:native("THREE.ShapeGeometry")
-extern class ShapeGeometry {
+extern class ShapeGeometry extends Geometry {
     public function new(shapes:Array<Shape>, options:Dynamic) : Void;
 }
