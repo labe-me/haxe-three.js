447b12115e5f5313bacc951f8695218d3aa145f0
Laurent Bédubourg <lbedubourg@gmail.com>
2015-03-18 22:36:04 +0100

    Update RingGeometry.hx
    Fixes #9

--- js/three/RingGeometry.hx
+++ js/three/RingGeometry.hx
@@ -1,6 +1,6 @@
 package js.three;
 
 @:native("THREE.RingGeometry")
-extern class RingGeometry {
+extern class RingGeometry extends Geometry {
     public function new(?innerRadius:Float, ?outerRadius:Float, ?thetaSegments:Int, ?phiSegments:Int, ?thetaStart:Float, ?thetaLength:Float) : Void;
 }
